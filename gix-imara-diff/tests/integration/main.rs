bc8ed5c78d739d51d1dfd5b95f73b5d3b9252969
Sebastian Thiel <sebastian.thiel@icloud.com>
2026-04-07 12:07:41 +0800

    Make vendored, fixed imara-diff crates part of the workspace

--- gix-imara-diff/tests/integration/main.rs
+++ gix-imara-diff/tests/integration/main.rs
@@ -6,10 +6,10 @@ use expect_test::{expect, expect_file};
 // use git::bstr::BStr;
 // use git_repository as git;
 
-use imara_diff::sources::words;
-use imara_diff::BasicLineDiffPrinter;
-use imara_diff::InternedInput;
-use imara_diff::{Algorithm, Diff, UnifiedDiffConfig};
+use gix_imara_diff::sources::words;
+use gix_imara_diff::BasicLineDiffPrinter;
+use gix_imara_diff::InternedInput;
+use gix_imara_diff::{Algorithm, Diff, UnifiedDiffConfig};
 
 const ALL_ALGORITHMS: [Algorithm; 2] = [Algorithm::Histogram, Algorithm::Myers];
 
@@ -206,8 +206,8 @@ i
 mod latin_word_diff {
     use crate::ALL_ALGORITHMS;
 
-    use imara_diff::sources::words;
-    use imara_diff::{Diff, InternedInput, Token};
+    use gix_imara_diff::sources::words;
+    use gix_imara_diff::{Diff, InternedInput, Token};
     use std::mem::swap;
     use std::ops::Range;
 
